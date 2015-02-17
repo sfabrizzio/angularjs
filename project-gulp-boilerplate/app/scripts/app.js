70b9ed3bd9d20f515425ced934331da35e2601e8
Jose Diaz <josediaz@Joses-MacBook-Pro.local>
2015-02-17 18:54:33 -0500

    demo con gulp boilerplate

---
+++ project-gulp-boilerplate/app/scripts/app.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+  // Aquí irá la funcionalidad
+
+  angular.module('blog', ['ngRoute', 'blog.controllers', 'blog.services', 'blog.templates'])
+         .config(config);
+
+  function config ($locationProvider, $routeProvider) {
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/post-list.tpl.html',
+        controller: 'PostListController',
+        controllerAs: 'postlist'
+      })
+      .when('/post/:postId', {
+        templateUrl: 'views/post-detail.tpl.html',
+        controller: 'PostDetailController',
+        controllerAs: 'postdetail'
+      })
+      .when('/new', {
+        templateUrl: 'views/post-create.tpl.html',
+        controller: 'PostCreateController',
+        controllerAs: 'postcreate'
+      });
+
+  }
+
+
+
+})();
